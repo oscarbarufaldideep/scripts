0ce0160abf396947bfb723355c07f60c90542e53
oscarbarufaldideep <138253051+oscarbarufaldideep@users.noreply.github.com>
2023-11-21 14:38:20 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,7 +5,6 @@ sudo snap install ngrok
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 sudo apt install unzip
 unzip ngrok-stable-linux-amd64.zip
-./ngrok authtoken 2IGw6j4SAy90W3sGDa7hUnJXkEU_o8HXsAHgke9oGH232XsG
 ./ngrok tcp 22
 
 
